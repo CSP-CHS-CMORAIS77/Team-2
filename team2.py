3684d0f3d97ca5d9bfaf4f07e3e9a39da2f19b64
NatePerr <45546426+NatePerr@users.noreply.github.com>
2018-12-04 09:06:40 -0500

    Revert "Revert "Merge pull request #1 from CSP-CHS-CMORAIS77/testing""
    This reverts commit f30a083da653be1845d8c10db4286587cfa5e1e9.

--- team2.py
+++ team2.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Hanging Rock' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'p'
+strategy_description = 'It decides with an input.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
